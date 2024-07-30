e799e26ddb54eb10ddc4245f62882d2d5b9cc767
WeslleyNsou <120567284+WeslleyNsou@users.noreply.github.com>
2024-07-30 07:27:28 -0300

    Exercicios da semana

---
+++ Aula-011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro e de ${vel} km/h`) 
+if (vel > 60){
+    console.log('Voce utrapasou a velocidade permitida. Multado!')
+}
+console.log('Lembre sempre de usar sinto de segurança.')
---
+++ ex009.js
@@ -0,0 +1,10 @@
+var pais = 'brasil'
+
+console.log(`Voce vive em ${pais}`)
+
+if (pais != 'Brasil'){
+    console.log(`Voce é Estrangeiro!`)
+}
+else{
+    console.log(`Voce é Brasileiro!`)
+}
