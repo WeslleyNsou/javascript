04b613f384d3cc155cfbb4d9d7296d69ae99d81b
WeslleyNsou <120567284+WeslleyNsou@users.noreply.github.com>
2024-08-21 10:45:26 -0300

    verifica'cao de preenchimento

--- Aula-12ex/ex0015/script.js
+++ Aula-12ex/ex0015/script.js
@@ -1,16 +1,24 @@
 function verificar(){
-
-    //captura de ano atual
+    //Capturando data atual
     var date = new Date()
-    var ano = date.getFullYear()
-    //capturando ano nacimento
-    var anof = document.getElementById('txidade')
-    
+    var aano = date.getFullYear()
+    //Capturando data do formulario
+    var anof = document.getElementById('anof')
     var res = document.getElementById('res')
 
-    if( txidade.Value.length == 0 || txidade.value > ano){
-        alert('[erro] verificar novamente o ano')
+    if (anof.value.length == 0 || Number(anof.value) >= aano){
+        alert(`[ERRO] tente novamente!`)
     }else{
-        alert('POSITIVO')
+        var sex = document.getElementsByName('raid')
+        var idade = aano - Number(anof.value)
+        var genero = ''
+
+        if(sex[0].checked){
+            genero = 'Homen'
+        }else{
+            genero = 'Mulher'
+        }
+
+        res.innerHTML= `Sua idade e de ${idade} anos`
     }
 }
