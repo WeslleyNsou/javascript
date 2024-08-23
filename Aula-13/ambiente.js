6987ded722d460766ff2a2c2032506c94941f772
WeslleyNsou <120567284+WeslleyNsou@users.noreply.github.com>
2024-08-23 10:15:17 -0300

    Create ambiente.js

---
+++ Aula-13/ambiente.js
@@ -0,0 +1,25 @@
+var c = 1
+// REALIZA O TEXTE NO FINAL (E MAIS LENTO PARA EXECULTAR)
+do {
+    console.log(`passo ${c}`)
+    c++
+}while ( c <= 6 )
+
+/*
+
+    FAZ O TEXTE NO INICIO
+
+while( c <= 6){
+    console.log(`passo ${c}`)
+    c++
+}
+*/
+
+/*
+Console.log(`passo 1`)
+Console.log(`passo 2`)
+Console.log(`passo 3`)
+Console.log(`passo 4`)
+Console.log(`passo 5`)
+Console.log(`passo 6`)
+*/
