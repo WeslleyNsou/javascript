0f818822ed2d2fb3b952a04b31d47eb09fee5974
WeslleyNsou <120567284+WeslleyNsou@users.noreply.github.com>
2024-08-31 10:05:59 -0300

    a

--- Aula-14ex/ex016/script.js
+++ Aula-14ex/ex016/script.js
@@ -1,16 +1,21 @@
-var ni = document.getElementById(`ni`)
-var nf = document.getElementById(`nf`)
-var pass = document.getElementById(`pass`)
 
 function contar(){
+    var ni = document.getElementById(`ni`)
+    var nf = document.getElementById(`nf`)
+    var pass = document.getElementById(`pass`)
+    var res = document.getElementById(`res`)
 
-    if(ni.value.length == 0 || nf.value.length == 0){
-        alert(`[ERRO] e necessario digitar um numero!`)
+    if(ni.value.length == 0 || nf.value.length == 0 || pass.value,length == 0){
+        alert(`[ERRO] faltam dados!`)
+    } else{
+        res.innerHTML = 'contandor: '
+        var i = Number(ni.value)
+        var f = Number(nf.value)
+        var pass = Number(pass.value)
+
+        for( var c = i; c <= f; c += p){
+            res.innerHTML += `${c}`
+        }
     }
 
-    for(ni ; nf <= ni; ni += pass) {
-        console.log(ni)
-    }
-
-    res.innerHTML = ${'ni'}
 }
