464d305ab7c9296b012ce8dd5863bed7508b2b86
WeslleyNsou <120567284+WeslleyNsou@users.noreply.github.com>
2024-08-31 11:06:08 -0300

    ex16 resolvido

--- Aula-14ex/ex016/script.js
+++ Aula-14ex/ex016/script.js
@@ -5,17 +5,28 @@ function contar(){
     var pass = document.getElementById(`pass`)
     var res = document.getElementById(`res`)
 
-    if(ni.value.length == 0 || nf.value.length == 0 || pass.value,length == 0){
+
+    if(ni.value.length == 0 || nf.value.length == 0 || pass.value.length == 0){
         alert(`[ERRO] faltam dados!`)
+        res.innerHTML = `Impossivel contar`
     } else{
-        res.innerHTML = 'contandor: '
+        res.innerHTML = 'contandor: <br>'
         var i = Number(ni.value)
         var f = Number(nf.value)
-        var pass = Number(pass.value)
+        var p = Number(pass.value)
 
+        if (p <= 0){
+            alert(`[ERRO] passo invalido! Passo valera 1`)
+            p = 1
+        }
+    } if (i < f){
         for( var c = i; c <= f; c += p){
-            res.innerHTML += `${c}`
+            res.innerHTML += `${c} \u{1f449}`
+    }
+    } else{
+        for (var c = i; c >= f; c -= p){
+            res.innerHTML += `${c} \u{1f449}`
         }
     }
-
+    res.innerHTML += `\u{1f3c1}`
 }
