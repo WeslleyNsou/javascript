8e090dc392e715ff3c563439b636ed1937e88331
WeslleyNsou <120567284+WeslleyNsou@users.noreply.github.com>
2024-09-26 10:39:34 -0300

    probleme

--- Aula-16ex/ambiente.js
+++ Aula-16ex/ambiente.js
@@ -1,17 +1,26 @@
-function add(){
-    var num = document.getElementById(`num`) 
-    var res = document.getElementById(`res`)
+var num = document.getElementById(`num`) 
+var selcf = document.getElementById(`selcf`)
+var res = document.getElementById(`res`)
+var valor = []
 
-    if(num.value.length == 0){
-        alert('[ERRO!] Digite um numero.')
-    } else{
-        for(var con = 0; con <=100; con++){
-            var n = Number(num.value)
-    
-            res.innerText = ``
-            var item = document.createElement('Option')
-            item.text = `Número ${n} adicionado!`
-            res.appendChild(item)
-        }
+function isnum(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    }else{
+        return false
     }
+}
+function inlist(n, l){
+    if(l.indexof(Number(n)) != -1 ){
+        return true
+    }else{
+        return false
+    }
+}
+
+function add(){
+
+    if(isnum(num.value) && !inlist(num.value, valor)){
+        alert(`tudo ok`)
+    } 
 }
