47b33ecd16c5e52368e83fe120332dcd41a8f04b
WeslleyNsou <120567284+WeslleyNsou@users.noreply.github.com>
2024-10-04 10:01:32 -0300

    inciando o processo de interatividade

--- Aula-16ex/ambiente.js
+++ Aula-16ex/ambiente.js
@@ -1,26 +1,37 @@
-var num = document.getElementById(`num`) 
-var selcf = document.getElementById(`selcf`)
+var num = document.getElementById(`num`)
 var res = document.getElementById(`res`)
-var valor = []
+var lista = document.getElementById(`selecf`)
+var valores = []
 
-function isnum(n){
-    if(Number(n) >= 1 && Number(n) <= 100){
+//Verifica se o numero esta de 1 a 100
+function numero(n){
+    if( Number(n) >= 1 && Number(n) <= 100){
         return true
     }else{
         return false
     }
 }
+//Verifica se o numero estar na lista por meio de um indexOf, no qual se o numero não for encontrado sera mostrado -1
 function inlist(n, l){
-    if(l.indexof(Number(n)) != -1 ){
+    if( l.indexOf(Number(n)) != -1){
         return true
     }else{
         return false
     }
+
 }
 
 function add(){
+    if(numero(num.value) && !inlist(num.value, valores)){
+        valores.push(Number(num.value))
 
-    if(isnum(num.value) && !inlist(num.value, valor)){
-        alert(`tudo ok`)
-    } 
+        var item = document.createElement(`option`)
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+    }else{
+        alert(`Valor invalido ou não encontrado na lista !`)
+    }
+    //deixa a caixa vazia após adicionar um numero
+    num.value = ``
+    num.focus()
 }
